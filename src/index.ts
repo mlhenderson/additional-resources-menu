89c440cdade8e9832fcdfadf122f20b3b5200283
Josh Geden <joshgeden10@gmail.com>
2021-05-10 13:13:23 -0400

    added more comments and reorganized code

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 // partially from https://github.com/timkpaine/jupyterlab_iframe/blob/main/js/src/index.ts
 // used to open website in a jupyter tab
+// default settings do not use this widget, just a window.open()
 let unique = 0;
 class IFrameWidget extends Widget {
   public constructor(title: string, path: string) {
@@ -72,60 +73,69 @@ const extension: JupyterFrontEndPlugin<void> = {
     console.log(`JupyterLab extension ${PLUGIN_ID} is activated!`);
     const { commands } = app;
 
+    // initialize links and try to load them from the settings file
     let links = [];
 
     const settings = await settingRegistry.load(PLUGIN_ID);
     links = settings.get('links').composite as any;
 
+    // if we could not load the links from settings return with an error
     if (links === [] || Object.keys(links).length === 0) {
       console.error(`${PLUGIN_ID} No links are set in overrides.json`);
       return;
     }
 
+    // create the menu that will be added to the help menu
     const additionalResourcesMenu: Menu = new Menu({ commands });
     additionalResourcesMenu.title.label = settings.get('menu-title')
       .composite as string;
 
-    // Loop through links and add each as a window.open() command
+    // Loop through links and add each as a window.open() command or to open as IFrame
     links.forEach((link: { name: string; url: string }) => {
       const command = `open-${link.name}`;
       commands.addCommand(command, {
         label: `${link.name}`,
         caption: `${link.name}`,
         execute: async () => {
-          // use widget to open in new Jupyter tab
-          const widget = new IFrameWidget(link.name, link.url);
-          const response = await widget.createIFrame(link.name, link.url);
-
           // Get settings on whether or not to open page in jupyter tab
           const openInJupyter = settings.get('open-in-jupyter')
             .composite as boolean;
 
-          // check if the IFrame was created correctly
-          // if so open it in a jupyter notebook tab
-          // otherwise open it in a browser tab
-          if (response && openInJupyter) {
-            app.shell.add(widget, 'main');
-            app.shell.activateById(widget.id);
+          // if we should open in jupyter tab, try to do so, otherwise use window.open()
+          if (openInJupyter) {
+            // use widget to open in new Jupyter tab
+            const widget = new IFrameWidget(link.name, link.url);
+            const response = await widget.createIFrame(link.name, link.url);
 
-            // find the links tracker and add the new widget to be tracked
-            trackers.forEach(t => {
-              if (response && t.name === link.name) {
-                if (!t.tracker.has(widget)) {
-                  t.tracker.add(widget);
+            // check if the IFrame was created correctly
+            // if so open it in a jupyter notebook tab
+            // otherwise open it in a browser tab
+            if (response && openInJupyter) {
+              app.shell.add(widget, 'main');
+              app.shell.activateById(widget.id);
+
+              // find the links tracker and add the new widget to be tracked
+              trackers.forEach(t => {
+                if (response && t.name === link.name) {
+                  if (!t.tracker.has(widget)) {
+                    t.tracker.add(widget);
+                  }
                 }
-              }
-            });
+              });
+            } else {
+              window.open(link.url);
+            }
           } else {
             window.open(link.url);
           }
         }
       });
 
-      // add each command to the help menu
+      // add each command to the additional resources menu
       additionalResourcesMenu.addItem({ command });
     });
 
+    // trackers are only used when opening window in a jupyter tab
     // Create a tracker for every link
     const trackers: any[] = [];
     links.forEach((link: { name: string }) => {
@@ -143,7 +153,7 @@ const extension: JupyterFrontEndPlugin<void> = {
       });
     });
 
-    // add help menu as a submenu of the help menu
+    // add additional resources menu as a submenu of the help menu
     mainMenu.helpMenu.addGroup(
       [
         {
