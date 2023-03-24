1ac23eabce6ea32f089d3e257324c0c98a15441c
Matt Henderson <mhenderson@lbl.gov>
2023-03-23 17:22:56 -0700

    remove unused imports

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,13 +1,8 @@
-import pytest
 import time
-import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
 # This test file assumes that overrides.json is set to the following information:
